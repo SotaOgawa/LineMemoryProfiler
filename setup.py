cfde5752551cc5a3d57921e61bc70579a518d0cf
SotaOgawa <ogawa.s.ay@m.titech.ac.jp>
2024-12-02 19:04:22 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LineMemoryProfiler',
+    version='0.1.0',
+    description='',
+    author='Sota Ogawa',
+    author_email='ogawa.s.ay@mdl.c.titech.ac.jp',
+    url='https://github.com/SotaOgawa/LineMemoryProfiler',
+    packages=find_packages(),
+    python_requires='>=3.8',
+)
